9e3258e6682fce37c495fdf616618a2bb75c51f3
Kyle Chivers <chiverskyle@gmail.com>
2019-09-09 18:48:45 -0700

    Updated to load items from plist instead of array in memory.

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -18,20 +18,7 @@ class TodoListViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        print(dataFilePath)
-        
-        let newItem = Item()
-        newItem.title = "Find Mike"
-        itemArray.append(newItem)
-        
-        let newItem2 = Item()
-        newItem2.title = "Find Mike Again"
-        itemArray.append(newItem2)
-        
-        let newItem3 = Item()
-        newItem3.title = "Found Mike, Woohoo!"
-        itemArray.append(newItem3)
-        
+        loadItems()
     }
     
     //MARK: - Tableview Datasource Methods
@@ -106,6 +93,17 @@ class TodoListViewController: UITableViewController {
         }
         
         self.tableView.reloadData()
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding item array, \(error)")
+            }
+        }
     }
     
 }
