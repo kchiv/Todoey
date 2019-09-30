0c3b6fe47610f53643aeea534926a789801c127a
Kyle Chivers <chiverskyle@gmail.com>
2019-09-29 20:23:00 -0700

    Resigned search bar as first responder.

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -131,6 +131,11 @@ extension TodoListViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         if searchBar.text?.count == 0 {
             loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+
         }
     }
     
