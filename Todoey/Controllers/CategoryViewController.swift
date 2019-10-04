01678e716a4264e6748cb6bdfae7aa0e4a108323
Kyle Chivers <chiverskyle@gmail.com>
2019-10-03 20:21:35 -0700

    Set up new category table view.

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Kyle Chivers on 10/1/19.
+//  Copyright © 2019 Kyle Chivers. All rights reserved.
+//
+
+import UIKit
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+}
