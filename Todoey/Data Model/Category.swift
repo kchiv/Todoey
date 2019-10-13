f00ee647ba0eaf1bc27a3da6ebeedc06a108069e
Kyle Chivers <chiverskyle@gmail.com>
2019-10-13 14:40:34 -0700

    Setting up data models for Realm.

---
+++ Todoey/Data Model/Category.swift
@@ -0,0 +1,15 @@
+//
+//  Category.swift
+//  Todoey
+//
+//  Created by Kyle Chivers on 10/13/19.
+//  Copyright © 2019 Kyle Chivers. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Category: Object {
+    @objc dynamic var name: String = ""
+    let items = List<Item>()
+}
